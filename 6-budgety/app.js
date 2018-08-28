3b9cc5afa960dd20db6412a03330799d2824ac64
Cindy <shwoo@olimplanet.com>
2018-08-28 17:44:56 +0900

    Chapter06 - 96

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -12,7 +12,7 @@ var budgetController = (function() {
     if (totalIncome > 0) {
       this.percentage = Math.round((this.value / totalIncome) * 100);
     } else {
-      this.percentage = -1
+      this.percentage = -1;
     }
   };
 
@@ -26,6 +26,14 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(cur) {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
@@ -34,60 +42,65 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
     addItem: function(type, des, val) {
-        var newItem, ID;
+      var newItem, ID;
 
-        // Create new ID
-        if (data.allItems[type].length > 0) {
-          ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-        } else {
-          ID = 0;
-        }
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
 
-        // Create new item based on 'inc' or 'exp' type
-        if (type === 'exp') {
-          newItem = new Expense(ID, des, val);
-        } else if (type === 'inc') {
-          newItem = new Income(ID, des, val);
-        }
+      // Create new item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
 
-        // Push it into our data structure
-        data.allItems[type].push(newItem);
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
 
-        // Return the new element
-        return newItem;
+      // Rreturn the new element
+      return newItem;
     },
 
-<<<<<<< HEAD
     deleteItem: function(type, id) {
       var ids, index;
 
       // id = 6
-      // data.allItems[type][id];
-      // ids = [ 1 2 4 6 8 ]
-      // index = 3
+      //data.allItems[type][id];
+      // ids = [1 2 4 6 8]
+      //index = 3
 
-      ids = data.allItems[type].map(current => current.id);
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+
       index = ids.indexOf(id);
 
       if (index !== -1) {
         data.allItems[type].splice(index, 1);
       }
+
     },
 
     calculateBudget: function() {
-      // calculate total income and expenses
+      // Calculate total income and expenses
       calculateTotal('exp');
       calculateTotal('inc');
 
-      // calculate the budget: income - expenses
-      data.budget = data.totals.inc - data.totals.exp
+      // Calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
 
-      // calculate the percentage of income that we spent
+      // Calculate the percentage of income that we spent
       if (data.totals.inc > 0) {
         data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
       } else {
@@ -98,19 +111,20 @@ var budgetController = (function() {
     },
 
     calculatePercentages: function() {
+
       /*
       a = 20
       b = 10
       c = 40
       income = 100
-      a = 20 / 100 = 20%
-      b = 10 / 100 = 10%
-      c = 40 / 100 = 40%
+      a = 20/100 = 20%
+      b = 10/100 = 10%
+      c = 40/100 = 40%
       */
 
       data.allItems.exp.forEach(function(cur) {
         cur.calcPercentage(data.totals.inc);
-      })
+      });
     },
 
     getPercentages: function() {
@@ -129,14 +143,13 @@ var budgetController = (function() {
       };
     },
 
-=======
->>>>>>> parent of 23be31f... Chapter06 - 2차
     testing: function() {
       console.log(data);
     }
-  };
+  }
 
 })();
+
 
 
 // UI CONTROLLER
@@ -148,85 +161,40 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-<<<<<<< HEAD
     expensesContainer: '.expenses__list',
     budgetLabel: '.budget__value',
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage',
-    dateLabel: '.budget__title--month'
-  };
-
-  var formatNumber = function(num, type) {
-    var numSplit, int, dec, type;
-    /*
-    + or - before number
-    exactly 2 decimal points
-    comma separating the thousands
-
-    2310.4567 -> + 2,310.46
-    2000 -> 2,000.00
-    */
-
-    num = Math.abs(num);
-    num = num.toFixed(2);
-
-    numSplit = num.split('.');
-
-    int = numSplit[0];
-    if (int.length > 3) {
-      int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, int.length); // input 2310, output 2,310
-    }
-
-    dec = numSplit[1];
-
-<<<<<<< HEAD
-    return (type === 'exp' ? '-' : '+') + ' ' + int + dec;
-=======
-    return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
->>>>>>> parent of dbccd08... Revert "Chapter06 - 2차"
-  };
-
-  var nodeListForEach = function(list, callback) {
-    for (var i = 0; i < list.length; i++) {
-      callback(list[i], i);
-    }
-  };
-=======
-    expensesContainer: '.expenses__list'
+    expensesPercLabel: '.item__percentage'
   }
->>>>>>> parent of 23be31f... Chapter06 - 2차
 
   return {
     getInput: function() {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       }
     },
 
     addListItem: function(obj, type) {
       var html, newHtml, element;
-
       // Create HTML string with placeholder text
 
       if (type === 'inc') {
         element = DOMstrings.incomeContainer;
-
         html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else if (type === 'exp') {
         element = DOMstrings.expensesContainer;
-
-        html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
       }
 
       // Replace the placeholder text with some actual data
       newHtml = html.replace('%id%', obj.id);
       newHtml = newHtml.replace('%description%', obj.description);
-      newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
+      newHtml = newHtml.replace('%value%', obj.value);
 
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
@@ -238,38 +206,36 @@ var UIController = (function() {
     },
 
     clearFields: function() {
-      var fields;
-      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
-<<<<<<< HEAD
-      fieldsArr = Array.prototype.slice.call(fields);
+      var fields, fieldsArr;
 
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+      fieldsArr = Array.prototype.slice.call(fields);
       fieldsArr.forEach(function(current, index, array) {
         current.value = "";
       });
-
       fieldsArr[0].focus();
     },
 
     displayBudget: function(obj) {
-      var type;
-      obj.budget > 0 ? type = 'inc' : type = 'exp';
-
-      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
-      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
-      document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
 
       if (obj.percentage > 0) {
         document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
       } else {
         document.querySelector(DOMstrings.percentageLabel).textContent = '---';
       }
-=======
->>>>>>> parent of 23be31f... Chapter06 - 2차
     },
 
     displayPercentages: function(percentages) {
 
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+      var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
 
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0) {
@@ -277,32 +243,8 @@ var UIController = (function() {
         } else {
           current.textContent = '---';
         }
-      });
-    },
+      })
 
-    displayMonth: function() {
-      var now, months, month, year;
-      now = new Date();
-      // var christmas = new Date(2018, 11, 25);
-      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-      month = now.getMonth();
-      year = now.getFullYear();
-
-      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
-    },
-
-    changedType: function() {
-      var fields = document.querySelectorAll(
-        DOMstrings.inputType + ',' +
-        DOMstrings.inputDescription + ',' +
-        DOMstrings.inputValue
-      );
-
-      nodeListForEach(fields, function(cur) {
-        cur.classList.toggle('red-focus');
-      });
-
-      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
     },
 
     getDOMstrings: function() {
@@ -321,15 +263,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-    document.addEventListener('keypress', function(event) {
-      if (event.keyCode === 13 || event.which === 13) {
+    document.addEventListener('keypress', function(e) {
+      if (e.keyCode === 13 || e.which === 13) {
         ctrlAddItem();
       }
     });
 
-<<<<<<< HEAD
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
-    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
   };
 
@@ -342,8 +282,6 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 3. Display the budget on the UI
     UICtrl.displayBudget(budget);
-=======
->>>>>>> parent of 23be31f... Chapter06 - 2차
   };
 
   var updatePercentages = function() {
@@ -355,22 +293,22 @@ var controller = (function(budgetCtrl, UICtrl) {
     var percentages = budgetCtrl.getPercentages();
 
     // 3. Update the UI with the new percentages
-    UICtrl.displayPercentages(percentages);
+    console.log(percentages)
   };
 
   var ctrlAddItem = function() {
-    var input, newItem;
+    var input, newItem
 
-    // 1. Get the filed input data
+    // 1. Get the field input data
     input = UICtrl.getInput();
 
-    // 2. Add the item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+      // 2. Add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value)
 
-    // 3. Add the item to the UI
-    UICtrl.addListItem(newItem, input.type);
+      // 3. Add the item to the UI
+      UICtrl.addListItem(newItem, input.type);
 
-<<<<<<< HEAD
       // 4. Clear the fields
       UICtrl.clearFields();
 
@@ -382,10 +320,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     }
   };
 
-  var ctrlDeleteItem = function(event) {
+  var ctrlDeleteItem = function(e) {
     var itemID, splitID, type, ID;
 
-    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+    itemID = e.target.parentNode.parentNode.parentNode.parentNode.id;
 
     if (itemID) {
       // inc-1
@@ -405,29 +343,20 @@ var controller = (function(budgetCtrl, UICtrl) {
       // 4. Calculate and update percentages
       updatePercentages();
     }
-=======
-    // 4. Calculate the budget
-
-    // 5. Display the budget on the UI
->>>>>>> parent of 23be31f... Chapter06 - 2차
   };
 
   return {
     init: function() {
-      console.log('Applicaton has started.');
-<<<<<<< HEAD
-      UICtrl.displayMonth();
+      console.log('Application has started.');
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
         totalExp: 0,
         percentage: -1
       });
-=======
->>>>>>> parent of 23be31f... Chapter06 - 2차
       setupEventListeners();
     }
-  };
+  }
 
 })(budgetController, UIController);
 
