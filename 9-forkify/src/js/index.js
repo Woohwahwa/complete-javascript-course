ce63da78836a858c024b7f4cd88da92ac4165d4f
Cindy <shwoo@olimplanet.com>
2018-09-24 19:41:48 +0900

    Chapter09 ~ 140

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,5 +1,32 @@
-import str from './models/Search';
-// import { add as a, multiply as m, ID } from './views/searchView';
-import * as searchView from './views/searchView'
+import Search from './models/Search';
 
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query from view
+  const query = 'pizza' // todo
+
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for results
+
+    // 4) Search for recipes
+    await state.search.getResults();
+
+    // 5) Render results on UI
+    console.log(state.search.result)
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+})
