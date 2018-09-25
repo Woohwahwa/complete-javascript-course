a645c56afc1508d2cdfef3523f64c3fa10be3e9e
Cindy <shwoo@olimplanet.com>
2018-09-26 00:47:22 +0900

    Chapter09 ~ 145

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -10,6 +11,7 @@ import { elements, renderLoader, clearLoader } from './views/base';
 */
 const state = {};
 
+/** SEARCH CONTROLLER **/
 const controlSearch = async () => {
   // 1) Get query from view
   const query = searchView.getInput();
@@ -35,4 +37,18 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
-})
+});
+
+elements.searchResPages.addEventListener('click', e => {
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+});
+
+/** RECIPE CONTROLLER **/
+const r = new Recipe(46956);
+r.getRecipe();
+console.log(r);
